c20ac682ee948f8592f2b046aecb8e0ca503c806
tdzz1102 <wasuremono127@gmail.com>
2023-04-20 10:28:17 +0800

    use redis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 import sys
-from requests import Session
+import redis
+import requests
+import configparser
+from pathlib import Path
 from loguru import logger
 from base64 import b64decode
 from urllib.parse import urlsplit
@@ -10,38 +13,86 @@ from vmess import Vmess
 from settings import Settings
 
 
-logger.add(Settings.log_path)
+config = configparser.ConfigParser()
+config.read((Path(__file__).parent / 'config.ini').resolve())
+
+# logger.add(Settings.log_path)
 sub_content = ''
-# count = 0
 
 
-def update_sub():
+class RedisConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instanec._redis_client = redis.Redis(host='localhost', port=Settings.redis_port, db=Settings.redis_db, decode_responses=True)
+        return cls._instance
+
+    def get_redis_client(self) -> redis.client.Redis:
+        return self._redis_client
+
+
+rc = RedisConnection()
+
+
+def init_db():
+    r = rc.get_redis_client()
+    r.set('settings:sub_url', Settings.sub_url)
+    r.set('settings:v2ray_config_path', Settings.v2ray_config_path)
+    r.set('settings:log_path', Settings.log_path)
+    r.set('settings:docker', str(Settings.docker))
+    r.set('settings:container_name', Settings.container_name)
+    r.set('settings:ports', ','.join([str(x) for x in Settings.sub_url]))
+    r.close()
+
+
+def get_sub() -> bool:
     global sub_content
-    share_links = b64decode(sub_content).decode('utf-8').splitlines()
+    se = requests.Session()
+    se.trust_env = False
+    r = rc.get_redis_client()
+    sub_url = Settings.sub_url
+    new_sub_content = se.get(sub_url).text
+    if new_sub_content != sub_content:
+        logger.warning('Sub update detacted')
+        sub_content = new_sub_content
+        share_links = b64decode(sub_content).decode('utf-8').splitlines()
+        for i, share_link in enumerate(share_links):
+            url = urlsplit(share_link)
+            # ss or vmess
+            protocal = url.scheme 
+            # str or dict str
+            desc = b64decode(url.netloc + '===')
+            r.set(f'server:{i}:protocal', protocal)
+            r.set(f'server:{i}:desc', desc)
+            r.close()
+        return True
+    r.close()
+    return False
+    
 
+def update_config():
+    r = rc.get_redis_client()
+    n = len(r.keys('server:*:protocal'))
     try:
         with open(Settings.v2ray_config_path) as f:
             v2ray_config: dict = json.load(f)
     except:
-        print(
-            f'The config.json for v2ray not found in {Settings.v2ray_config_path}.')
+        logger.error(f'The config.json for v2ray not found in {Settings.v2ray_config_path}.')
         sys.exit(1)
 
     outbounds: list = v2ray_config['outbounds']
     outbounds.clear()
-
-    ss_items: list[ShadowSocks] = []
-    vmess_items: list[Vmess] = []
-
-    for share_link in share_links:
-        url = urlsplit(share_link)
-        protocal = url.scheme
-        tmp = b64decode(url.netloc + '===').decode('utf-8')
+    ss_items, vmess_items = [], []
+    for i in range(n):
+        protocal = r.get(f'server:{i}:protocal')
+        desc = r.get(f'server:{i}:desc')
         if protocal == 'ss':
-            ss_items.append(ShadowSocks(tmp))
+            ss_items.append(ShadowSocks(desc))
         elif protocal == 'vmess':
-            vmess_items.append(Vmess(json.loads(tmp)))
-
+            vmess_items.append(Vmess(json.loads(desc)))
+            
     outbounds.append(ShadowSocks.gen_outbound(ss_items))
     outbounds.append(Vmess.gen_outbound(vmess_items))
     logger.info('Subscription update OK.')
@@ -70,22 +121,15 @@ def update_sub():
                 name=Settings.container_name
             )
             logger.warning('No container found. Start a new one.')
+    r.close()
 
 
 def main():
-    se = Session()
-    se.trust_env = False
+    init_db()
     while True:
-        logger.info('Working...')
-        global sub_content
-        try:
-            new_sub_content = se.get(Settings.subscription_url).text
-            if new_sub_content != sub_content:
-                logger.warning('Detected subscription change.')
-                sub_content = new_sub_content
-                update_sub()
-        except Exception as e:
-            logger.error(str(e))
+        logger.info('working...')
+        if get_sub(): # 更新あり
+            update_config()
         sleep(60)
     
     
--- settings.py
+++ settings.py
@@ -1,8 +1,12 @@
 class Settings:
-    subscription_url = 'https://jmssub.net/members/getsub.php?service=719998&id=988bd515-6f99-409c-8ef6-a64fdf0f5e95'
-    v2ray_config_path = '/opt/jmssub/config.json'
-    log_path = '/opt/jmssub/log.log'
+    sub_url = 'https://jmssub.net/members/getsub.php?service=719998&id=988bd515-6f99-409c-8ef6-a64fdf0f5e95'
+    v2ray_config_path = '/root/jmssub-dev/config.json'
+    log_path = '/root/jmssub-dev/log.log'
+
+    redis_port = 6379
+    redis_db = 9
 
     docker = True
-    container_name = 'v2ray'
-    ports = [32700, 33414]
+    container_name = 'v2ray-dev'
+    ports = [34567, 45678]
+
